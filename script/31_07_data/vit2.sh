a90da78f8c2ad31ef6648dbcff702ecc690812e7
nhduong1203 <tien.nh205033@sis.hust.edu.vn>
2024-07-31 14:47:25 +0700

    update scripts

--- script/31_07_data/vit2.sh
+++ script/31_07_data/vit2.sh
@@ -1,4 +1,4 @@
 
-python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir 31_07_data --transform_groundtruth --_use_wandb --heads 8
-python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir 31_07_data --transform_groundtruth --_use_wandb --patch_size 5 --heads 8
-python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir 31_07_data --transform_groundtruth --_use_wandb --patch_size 20 --heads 8
+python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir cropped_data --transform_groundtruth --_use_wandb --heads 8
+python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir cropped_data --transform_groundtruth --_use_wandb --patch_size 5 --heads 8
+python repo/main2.py --model_type simple_vit  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir cropped_data --transform_groundtruth --_use_wandb --patch_size 20 --heads 8
