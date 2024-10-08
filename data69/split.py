50f728f5b0c3c2f6a878d5f7ab265df816c5940b
duclong1009 <duclong10092000@gmail.com>
2024-10-08 12:57:14 +0700

    data

--- data69/split.py
+++ data69/split.py
@@ -5,8 +5,8 @@ data_dir = "data69"
 df = pd.read_csv("data69/file_index69.csv")
 
 train_df=  df.values[:594]
-valid_df = df.values[594:765]
-test_df = df.values[765:]
+valid_df = df.values[594:766]
+test_df = df.values[766:]
 
 pd.DataFrame(train_df, columns=df.columns).to_csv(f"{data_dir}/train_index.csv")
 pd.DataFrame(valid_df, columns=df.columns).to_csv(f"{data_dir}/valid_index.csv")
