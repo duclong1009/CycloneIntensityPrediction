d1cafb2e3ca63d9dd12521823fe15be549a2f154
duclong1009 <duclong10092000@gmail.com>
2025-01-10 13:19:45 +0700

    script

---
+++ script/experiment/basedyear_data/data2/prompt6_freeze.sh
@@ -0,0 +1,28 @@
+# python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-7 --epochs 1000 --patience 10 --data_dir data27/cropped_data  --transform_groundtruth --loss_func mse  --_use_wandb
+
+
+
+### Tuning patch_size
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 1e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --transform_groundtruth --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
+python repo/main.py --model_type prompt_vit6_2  --batch_size 32 --lr 5e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data2 --_use_wandb --group_name prompt_vit6_79_yearbased_data2 --seed 79 --freeze
