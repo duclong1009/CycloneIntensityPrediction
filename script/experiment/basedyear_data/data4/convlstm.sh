47d3b1294fa35ae84906f3a436d91fe29717211b
user10 <user10@bcm-headnode01.cm.cluster>
2025-01-14 10:39:23 +0700

    script data 3/4

---
+++ script/experiment/basedyear_data/data4/convlstm.sh
@@ -0,0 +1,24 @@
+### Tuning patch_size
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 1e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-6 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-7 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-5 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --transform_groundtruth --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
+python repo/main.py --model_type convlstm1  --batch_size 32 --lr 5e-4 --epochs 1000 --patience 10 --data_dir /home/user01/aiotlab/longnd/cyclone_prediction/raw_data/basedyear_data/data4 --_use_wandb --group_name convlstm1_79_yearbased_data4 --seed 79
